3e300371ea296399cbefa7a723eb986791f5bb6a
dedy setiawan <dedyomek2005@gmail.com>
2022-08-09 15:31:28 +0800

    commit

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -3,10 +3,8 @@
 class Home extends Controller {
     public function index()
     {
-        // $data['judul'] = 'Home';
-        // $data['nama'] = $this->model('User_model')->getUser();
-        // $this->view('templates/header', $data);
-        // $this->view('home/index', $data);
-        // $this->view('templates/footer');
+        $this->view("templates/header");
+        $this->view("home/index");
+        $this->view("templates/footer");
     }
 }
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -3,23 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <title>Halaman <?= $data['judul']; ?></title>
-    <link rel="stylesheet" href="<?= BASEURL; ?>/css/bootstrap.css">
+    <link rel="stylesheet" href="<?= BASEURL; ?>/css/sb-admin-2.css">
     <link rel="stylesheet" href="<?= BASEURL; ?>/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
 </head>
-<body>
+<body class="base-color">
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container">
-      <a class="navbar-brand" href="<?= BASEURL; ?>">PHP MVC</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-item nav-link active" href="<?= BASEURL; ?>">Home <span class="sr-only">(current)</span></a>
-          <a class="nav-item nav-link" href="<?= BASEURL; ?>/mahasiswa">Mahasiswa</a>
-          <a class="nav-item nav-link" href="<?= BASEURL; ?>/about">About</a>
-        </div>
-      </div>
-  </div>
+<nav class="d-flex blue-gradient justify-content-between text-black fw-bold">
+ <h1 class="fw-bold">Quizcuy</h1>
+ <div class="icons d-flex">
+    <i class="bi bi-house-door-fill"></i>
+    <i class="bi bi-shop"></i>
+    <div class="rounded-full bg-primary p-4">
+
+    </div>
+ </div>
 </nav>
+<div class="base-color p-5">
+  <div class = "white rounded-x-20 p-4 mx-5"   
